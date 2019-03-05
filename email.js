45a7d8ac959fd21cbf353568eb5b5eba87fb8b1d
Amanda Baret <amandau@cowboyaliens.com>
2019-03-05 09:51:39 +0200

    invoice email

--- email.js
+++ email.js
@@ -16,9 +16,10 @@
 'use strict';
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
-//const QRCode = require('qrcode')
-var qr = require('qr-image');
-var fs = require('fs');
+const qr = require('qr-image');
+// const sendEmailConfirmationFunction = require('./sendEmailConfirmation');
+// const sendGuestEmailFunction = require('./sendGuestEmail');
+
 // Configure the email transport using the default SMTP transport and a GMail account.
 // For other types of transports such as Sendgrid see https://nodemailer.com/transports/
 // TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
@@ -32,40 +33,91 @@ const mailTransport = nodemailer.createTransport({
   },
 });
 
-// Sends an email confirmation when a user changes his mailing list subscription.
-exports.sendEmailConfirmation = functions.database.ref('/tickets/{userid}').onWrite((change) => {
-  const snapshot = change.after;
-  const val = snapshot.val();
-
-  const mailOptions = {
-    from: '"JaylaTickets" <noreply@firebase.com>',
-    to: val.email,
-  };
-
-  var imageData = qr.imageSync(val.reference, { type: 'png' });
-
-  const reference = 'Reference number: ' + val.reference;
-  const total = String(Number(val.tickets) * Number(val.price));
-  const numberOfTickets = Number(val.tickets) > 1? ' tickets at R': ' ticket for R';
-  const each = Number(val.tickets) > 1? ' each': '';
-  const message1 = 'You have successfully purchased ' + val.tickets + numberOfTickets + val.price + each;
-  const message2 = 'The total deducted from your account is R' + total;
-  const message3 = 'Please present the attached code at the venue door';
-  const from = 'With love from JaylaTickets xx'
-
-  // Building Email message.
-
-  const referenceNumber = val.reference;
-  mailOptions.subject = 'JaylaTickets Order confirmation, reference number: ' + val.reference;
-      mailOptions.html = '<strong>' + val.eventname +'</strong><br><strong>' + reference +'</strong></br><p>' + message1 +'</p><p>' + message2 +'</p><p>' + message3 +'</p><p>' + from +'</p>';
-      mailOptions.attachments = [  
-         {
-          filename: 'ticket.png',
-          content: imageData,
-          encoding: 'base64'
-        }
-      ]
-
-  return mailTransport.sendMail(mailOptions);
+   exports.sendEmailConfirmation = functions.database.ref('/tickets/{userid}').onWrite((change) => {
+    const snapshot = change.after;
+    const val = snapshot.val();
+  
+    const mailOptions = {
+      from: '"JaylaTickets" <noreply@firebase.com>',
+      to: val.email,
+      cc: val.ccemail
+    };
    
-});
+    var imageData = qr.imageSync(val.qrcode, { type: 'png' });
+    
+      const reference = 'Reference number: ' + val.reference;
+    
+      var title = 'Hi ' + val.holdername;
+     
+      var message1 = 'This is your electronic ticket for ' + val.eventname + ', priced at R' + String(val.price) + '.00';
+        
+      const message2 = 'Please present the attached code at the venue door';
+      const message3 = val.venueaddress;
+      const message4 = val.venuelatlong;
+      const message5 = 'From: ' + val.from + ' to: ' + val.to;
+      const from = 'With love from JaylaTickets xx'
+    
+      // Building Email message.
+    
+      const referenceNumber = val.reference;
+      mailOptions.subject = 'JaylaTickets Order confirmation, reference number: ' + val.reference;
+          mailOptions.html = '<strong>' + title +'</strong><br><strong>' + reference +'</strong><br><p>' + message1 +'</p><p>' + message2 +'</p><small>' + message3 +'</small><br><small>' + message4 +'</small><br><small>' + message5 +'</small><br><p>' + from +'</p>';
+          mailOptions.attachments = [  
+             {
+              filename: 'ticket.png',
+              content: imageData,
+              encoding: 'base64'
+            }
+          ]
+    
+      return mailTransport.sendMail(mailOptions);
+     
+  });
+
+  //exports.handler = function(mailTransport,change) {
+    exports.sendGuestEmail = functions.database.ref('/invoices/{reference}').onWrite((change) => {
+    const snapshot = change.after;
+    const val = snapshot.val();
+  
+    const mailOptions = {
+      from: '"Paati Passports" <noreply@firebase.com>',
+      to: val.email,
+    };
+   
+     var reference = "";
+
+      if(val.zapperRef)
+      {
+        reference = 'Zapper reference number: ' + val.zapperRef;
+      }
+      else
+      {
+        reference = 'PayFast reference number: ' + val.payFastRef;
+      }
+      
+     var message1 = Number(val.totalTickets) < 2? '1 ticket ' + val.total: val.totalTickets + ' tickets ';
+     message1 += ' to the value of R ' + val.total + '.00';
+     var message2 = "";
+     if(val.promocode)
+     {
+      message2 = 'The promotional code ' + val.promocode + ' was used to the value of R ' + val.promotionvalue + '.00';
+     }
+     else
+     {
+      message2 = 'No promotional code was used';
+     }
+      var message3 = 'The total deducted from your account is R ' + val.totalPaid;
+      const from = 'With love from Paati Passports xx'
+    
+      // Building Email message.
+    
+      mailOptions.subject = 'Paati Passports Ticket invoice';
+          mailOptions.html = '<strong>' + reference +'</strong><br><strong>' + val.eventname +'</strong></br><p>' + message1 +'</p><p>' + message2 +'</p><p>' + message3 +'</p><p>' + from +'</p>';
+       
+      return mailTransport.sendMail(mailOptions);
+     
+  });
+  
+   
+  
+  
