a6fcf2e8a0d825a4c7ac61f1ad0a716cb024dbe7
AmandaU <ursling@gmail.com>
2019-03-27 18:41:38 +0200

    removed local pay button

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -32,12 +32,9 @@
           </div>
 
         <!-- <creditcard  @eventname="updateparent"></creditcard> -->
-        <button v-show="isready" @click="saveTicketLocal()" >Buy</button><br>
-        <br>
-            <!-- <a href="https://sandbox.payfast.co.za/eng/process?cmd=_paynow&amp;receiver=10011455&amp;item_name=Event&amp;item_description=tickets&amp;amount=100.00&amp;return_url=http%3A%2F%2F192.168.8.107%3A8080%2F%23%2FSuccess%2Fid%3D12345&amp;cancel_url=http%3A%2F%2F192.168.8.107%3A8080%2F%23%2FCancel%2Fid%3D12345"><img src="https://www.payfast.co.za/images/buttons/dark-large-paynow.png" width="174" height="59" alt="Pay" title="Pay Now with PayFast" /></a> -->
-          <!-- <a v-show="isready" href="https://sandbox.payfast.co.za/eng/process?cmd=_paynow&amp;receiver=10011455&amp;item_name=Event&amp;item_description=tickets&amp;amount=100.00&amp;return_url=http%3A%2F%2F192.168.8.107%3A8080%2F%23%2FSuccess%2F%3Fid%3D12345&amp;cancel_url=http%3A%2F%2F192.168.8.107%3A8080%2F%23%2FCancel%2F%3Fid%3D12345"><img src="https://www.payfast.co.za/images/buttons/dark-large-paynow.png" width="174" height="59" alt="Pay" title="Pay Now with PayFast" /></a> -->
-          <!-- <a href="https://sandbox.payfast.co.za/eng/process?cmd=_paynow&amp;receiver=12581557&amp;item_name=Event&amp;item_description=tickets&amp;amount=100.00&amp;return_url=http%3A%2F%2F192.168.8.107%3A8080%2F%23%2FSuccess%2F%3Fticketid%3D12345%26pricebreakid%3D6789&amp;cancel_url=http%3A%2F%2F192.168.8.107%3A8080%2F%23%2FCancel%2F%3Fticketid%3D12345%26pricebreakid%3D6789"><img src="https://www.payfast.co.za/images/buttons/dark-large-paynow.png" width="174" height="59" alt="Pay" title="Pay Now with PayFast" /></a> -->
-    <div class="payColumn">
+        <!-- <button v-show="isready" @click="saveTicketLocal()" >Buy</button><br>
+        <br> -->
+     <div class="payColumn">
       <div class="payfastRow">
           <img src="../assets/PaymentMethods.png"  alt="Payment Methods"   style="margin: 10px;"/>  
 
@@ -46,12 +43,8 @@
               
       </div>
     </div>
-          <!-- <a  v-show="showZapperAppIcon" @click="saveTicket()"  v-bind:href="zapperUrl"><img src="../assets/ZapperLogo.png" width="200" height="113" alt="Pay" title="Zapper" /></a>
-         -->
-          <br>
-          <!-- <div id="Zapper" v-show="showZapperQRCode" ></div> -->
-
-           <div id="Zapper" ></div>
+           <br>
+          <div id="Zapper" ></div>
        </div>
    </div>
  
